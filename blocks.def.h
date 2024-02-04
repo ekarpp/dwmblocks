cd92cbf195344dd60a4a9cdb82263f4f98a0e84f
ekarpp <eetu@karppinen.xyz>
2024-02-04 22:05:13 +0200

    add volume

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
+	{"",		"vol_stats",			10,	0},
 	{"",		"brt_stats",			10,	0},
 	{"",		"bat_stats",			10,	0},
 	{"",		"gpu_stats",			10,	0},
